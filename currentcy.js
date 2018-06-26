cb808b92c018b215ef7586beb590d60cad1cfd48
tehw0lf <tehwolf@protonmail.com>
2018-06-26 22:12:59 +0200

    Reformat project with prettier
    In order to facilitate future work, prettier is now being used to format the code automatically.

--- currentcy.js
+++ currentcy.js
@@ -1,137 +1,142 @@
 function initRates() {
-    if (typeof rates === 'undefined') {
-        var rates = {
-            "EUR": {
-                "USD": ["0", "0"],
-                "CAD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "INR": ["0", "0"],
-                "JPY": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "USD": {
-                "EUR": ["0", "0"],
-                "CAD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "INR": ["0", "0"],
-                "JPY": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "CAD": {
-                "EUR": ["0", "0"],
-                "USD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "INR": ["0", "0"],
-                "JPY": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "GBP": {
-                "EUR": ["0", "0"],
-                "USD": ["0", "0"],
-                "CAD": ["0", "0"],
-                "INR": ["0", "0"],
-                "JPY": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "INR": {
-                "EUR": ["0", "0"],
-                "USD": ["0", "0"],
-                "CAD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "JPY": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "JPY": {
-                "EUR": ["0", "0"],
-                "USD": ["0", "0"],
-                "CAD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "INR": ["0", "0"],
-                "CNY": ["0", "0"],
-            },
-            "CNY": {
-                "EUR": ["0", "0"],
-                "USD": ["0", "0"],
-                "CAD": ["0", "0"],
-                "GBP": ["0", "0"],
-                "INR": ["0", "0"],
-                "JPY": ["0", "0"],
-            },
-        };
-        return rates;
-    } else {
-        console.log("ERROR: initRates() called with previously initialised rates.");
-    }
+  if (typeof rates === "undefined") {
+    var rates = {
+      EUR: {
+        USD: ["0", "0"],
+        CAD: ["0", "0"],
+        GBP: ["0", "0"],
+        INR: ["0", "0"],
+        JPY: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      USD: {
+        EUR: ["0", "0"],
+        CAD: ["0", "0"],
+        GBP: ["0", "0"],
+        INR: ["0", "0"],
+        JPY: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      CAD: {
+        EUR: ["0", "0"],
+        USD: ["0", "0"],
+        GBP: ["0", "0"],
+        INR: ["0", "0"],
+        JPY: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      GBP: {
+        EUR: ["0", "0"],
+        USD: ["0", "0"],
+        CAD: ["0", "0"],
+        INR: ["0", "0"],
+        JPY: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      INR: {
+        EUR: ["0", "0"],
+        USD: ["0", "0"],
+        CAD: ["0", "0"],
+        GBP: ["0", "0"],
+        JPY: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      JPY: {
+        EUR: ["0", "0"],
+        USD: ["0", "0"],
+        CAD: ["0", "0"],
+        GBP: ["0", "0"],
+        INR: ["0", "0"],
+        CNY: ["0", "0"]
+      },
+      CNY: {
+        EUR: ["0", "0"],
+        USD: ["0", "0"],
+        CAD: ["0", "0"],
+        GBP: ["0", "0"],
+        INR: ["0", "0"],
+        JPY: ["0", "0"]
+      }
+    };
+    return rates;
+  } else {
+    console.log("ERROR: initRates() called with previously initialised rates.");
+  }
 }
 
 function updateRates(rates) {
-    for (const currIn in rates) {
-        for (const currOut in rates[currIn]) {
-            if (Date.now() - Number(rates[currIn][currOut][1]) >= 1800000) { // 30 minutes
-                if (Date.now() - Number(rates[currOut][currIn][1]) < 1800000) {
-                    rates[currIn][currOut][0] = (1 / Number(rates[currOut][currIn][0])).toString(); // Value
-                    rates[currIn][currOut][1] = Number(rates[currOut][currIn][1]).toString(); // Timestamp
-                    browser.storage.local.set({
-                        rates
-                    });
-                } else {
-                    getRate(rates, currIn, currOut);
-                }
-            }
+  for (const currIn in rates) {
+    for (const currOut in rates[currIn]) {
+      if (Date.now() - Number(rates[currIn][currOut][1]) >= 1800000) {
+        // 30 minutes
+        if (Date.now() - Number(rates[currOut][currIn][1]) < 1800000) {
+          rates[currIn][currOut][0] = (
+            1 / Number(rates[currOut][currIn][0])
+          ).toString(); // Value
+          rates[currIn][currOut][1] = Number(
+            rates[currOut][currIn][1]
+          ).toString(); // Timestamp
+          browser.storage.local.set({
+            rates
+          });
+        } else {
+          getRate(rates, currIn, currOut);
         }
+      }
     }
+  }
 }
 
 function getRate(rates, currIn, currOut) {
-    var jsonSource = `https://free.currencyconverterapi.com/api/v5/convert?q=${currIn}_${currOut}&compact=y`;
-    try {
-        fetch(jsonSource)
-            .then(
-                function (res) {
-                    if (res.status !== 200) {
-                        console.log("Failed to fetch JSON: " + res.status);
-                        return;
-                    }
-                    res.json().then(function (data) {
-                        rates[currIn][currOut][0] = Object.values(data)[0].val.toString();
-                        rates[currIn][currOut][1] = Date.now().toString();
-                        browser.storage.local.set({
-                            rates
-                        });
-                        console.log(`rates for ${currIn} to ${currOut} set from ${jsonSource}.`);
-                    });
-                }
-            )
-            .catch(function (err) {
-                console.log(err);
-            });
-    } catch (err) {
-        console.log("Error: JSON data could not be read.");
-    }
+  var jsonSource = `https://free.currencyconverterapi.com/api/v5/convert?q=${currIn}_${currOut}&compact=y`;
+  try {
+    fetch(jsonSource)
+      .then(function(res) {
+        if (res.status !== 200) {
+          console.log("Failed to fetch JSON: " + res.status);
+          return;
+        }
+        res.json().then(function(data) {
+          rates[currIn][currOut][0] = Object.values(data)[0].val.toString();
+          rates[currIn][currOut][1] = Date.now().toString();
+          browser.storage.local.set({
+            rates
+          });
+          console.log(
+            `rates for ${currIn} to ${currOut} set from ${jsonSource}.`
+          );
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log("Error: JSON data could not be read.");
+  }
 }
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+  console.log(`Error: ${error}`);
 }
 
 function onGot(item) {
-    if (item.localCurr) {
-        localCurr = item.localCurr;
-    }
-    if (item.rates) {
-        rates = item.rates;
-        updateRates(rates);
-    } else {
-        rates = initRates();
-        updateRates(rates);
-    }
+  if (item.localCurr) {
+    localCurr = item.localCurr;
+  }
+  if (item.rates) {
+    rates = item.rates;
+    updateRates(rates);
+  } else {
+    rates = initRates();
+    updateRates(rates);
+  }
 }
 
 function main() {
-    var gettingCurr = browser.storage.local.get("localCurr");
-    gettingCurr.then(onGot, onError);
-    var gettingRates = browser.storage.local.get("rates");
-    gettingRates.then(onGot, onError);
+  var gettingCurr = browser.storage.local.get("localCurr");
+  gettingCurr.then(onGot, onError);
+  var gettingRates = browser.storage.local.get("rates");
+  gettingRates.then(onGot, onError);
 }
 
 main();
--- options.js
+++ options.js
@@ -1,22 +1,21 @@
 function saveOptions(e) {
-    e.preventDefault();
-    browser.storage.local.set({
-        localCurr: document.querySelector("#localCurr").value
-    });
+  e.preventDefault();
+  browser.storage.local.set({
+    localCurr: document.querySelector("#localCurr").value
+  });
 }
 
 function restoreOptions() {
+  function setCurrentChoice(result) {
+    document.querySelector("#localCurr").value = result.localCurr || "EUR";
+  }
 
-    function setCurrentChoice(result) {
-        document.querySelector("#localCurr").value = result.localCurr || "EUR";
-    }
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
 
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-
-    var getting = browser.storage.local.get("localCurr");
-    getting.then(setCurrentChoice, onError);
+  var getting = browser.storage.local.get("localCurr");
+  getting.then(setCurrentChoice, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
